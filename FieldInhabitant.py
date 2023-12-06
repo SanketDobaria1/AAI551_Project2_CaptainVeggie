a58a27240011fd15d3654064c4a4415a32cf2af7
Sameer Bhalala <68272275+SamirBhalala@users.noreply.github.com>
2023-12-05 19:23:00 -0500

    Create FieldInhabitant class

--- FieldInhabitant.py
+++ FieldInhabitant.py
@@ -1,2 +1,13 @@
 # Author: Sanket Dobaria, Sameer Bhalala
 # Date: 12/2/23
+
+
+class FieldInhabitant:
+    def _init_(self, symbol):
+        self._symbol = symbol
+
+    def get_symbol(self):
+        return self._symbol
+
+    def set_symbol(self, value):
+        self._symbol = value
