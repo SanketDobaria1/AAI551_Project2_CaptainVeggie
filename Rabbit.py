598f03c97d2e3e2105b1f974abc21cf7df38dfbf
Sameer Bhalala <68272275+SamirBhalala@users.noreply.github.com>
2023-12-05 19:26:05 -0500

    Create Rabbit Class

--- Rabbit.py
+++ Rabbit.py
@@ -1,2 +1,9 @@
 # Author: Sanket Dobaria, Sameer Bhalala
 # Date: 12/2/23
+
+
+from Creature import Creature
+
+class Rabbit(Creature):
+    def _init_(self, x, y):
+        super()._init_(x, y, "R")
