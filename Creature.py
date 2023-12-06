fa1610d42f6cfae0b0750c5c4faa64e961eb31a3
Sameer Bhalala <68272275+SamirBhalala@users.noreply.github.com>
2023-12-05 19:24:19 -0500

    Create Creature Class

--- Creature.py
+++ Creature.py
@@ -1,2 +1,23 @@
 # Author: Sanket Dobaria, Sameer Bhalala
 # Date: 12/2/23
+
+
+from FieldInhabitant import FieldInhabitant
+
+class Creature(FieldInhabitant):
+    def _init_(self, x, y, symbol):
+        super()._init_(symbol)
+        self._x = x
+        self._y = y
+
+    def getX(self):
+        return self._x
+
+    def setX(self, value):
+        self._x = value
+
+    def getY(self):
+        return self._y
+
+    def setY(self, value):
+        self._y = value
